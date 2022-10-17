78342a4833bb7efde9bace6b781e899a80ea3e50
KAW <36702039+kerry91@users.noreply.github.com>
2022-10-17 13:24:20 +0100

    .

--- script.js
+++ script.js
@@ -6,17 +6,98 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-/*
-function responsiveMenu() {
-  var x = document.getElementById("myTopnav");
-  let right = document.getElementById("nav-right");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+// Select increment and decrement buttons
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+
+// Select total count
+const totalCount = document.getElementById("count-result");
+
+// Variable to track count
+var count = 0;
+
+// Display initial count value
+totalCount.innerHTML = count;
+
+// Function to increment count
+function handleIncrement (){
+  count++;
+  totalCount.innerHTML = count;
+};
+
+// Function to decrement count
+function handleDecrement(){
+  if(count === 0){
+    return false
   }
+  else{
+    count--;
+  }
+
+  totalCount.innerHTML = count;
+};
+
+
+const button = document.getElementById("add2cart");
+const hideEmptyCartTxt = document.getElementById('emptyCartTxt')
+const box = document.getElementsByClassName("box");
+const ul = document.querySelector("ul");
+const dPrice = document.getElementById('d-price').textContent;
+const ePrice = dPrice * count.value;
+const cartCheckout = document.getElementById('cartCheckout');
+
+function createCartElement (){
+  const li = document.createElement("li"); //creates LI
+  var img = document.createElement("img");
+  var text = document.createElement("p");
+  var priceInfo = document.createElement("p");
+  var span = document.createElement("span"); // creates Span
+  var btn = document.createElement("button"); // creates button tag
+  var icon = document.createElement("i")
+  hideEmptyCartTxt.style.visibility = "hidden";
+  cartCheckout.style.visibility = "visible";
+  btn.setAttribute("class", "box");
+  icon.setAttribute("class", "fa-solid fa-trash-can");
+  img.setAttribute("src", "images/image-product-1-thumbnail.jpg");
+  text.setAttribute("class", "cartText");
+  text.innerHTML = "Fall Limited Edition...";
+  priceInfo.setAttribute("class", "cartPrice");
+  priceInfo.innerHTML = `${dPrice}  X ${count}   ${ePrice}`;
+  img.style.width = "50px";
+  img.style.height = "50px";
+  img.style.float = "left";
+  li.appendChild(span); // adds the span inside the LI
+  span.appendChild(img);
+  span.appendChild(text);
+  text.appendChild(priceInfo);
+  span.appendChild(btn); // adds the button inside the LI
+  btn.appendChild(icon);
+  ul.appendChild(li); // appends the LI to the UL
+
+
+  //Add functionality to remove the li item with the X button
+    for (var i = 0; i < box.length; i++) {
+      var delLi = box[i];
+      delLi.addEventListener("click", function(event) {
+        event.currentTarget.parentElement.remove();
+        hideEmptyCartTxt.style.visibility = "visible";
+        cartCheckout.style.visibility = "hidden";
+      });
+    }
+    console.log(ePrice, dPrice, count);
+  }
+
+  function addListAfterClick() {
+  let i = 1;
+  if (i = 1) {
+    createCartElement();
+  }
+  else{
+    console.log("error");
+  }
+
 }
-*/
+
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -81,37 +162,8 @@ window.onclick = function(event) {
   }
 }
 
-// Select increment and decrement buttons
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-
-// Select total count
-const totalCount = document.getElementById("count-result");
-
-// Variable to track count
-var count = 0;
-
-// Display initial count value
-totalCount.innerHTML = count;
-
-// Function to increment count
-function handleIncrement (){
-  count++;
-  totalCount.innerHTML = count;
-};
-
-// Function to decrement count
-function handleDecrement(){
-  if(count === 0){
-    return false
-  }
-  else{
-    count--;
-  }
-
-  totalCount.innerHTML = count;
-};
 
 // Add click event to buttons
 plus.addEventListener("click", handleIncrement);
 minus.addEventListener("click", handleDecrement);
+button.addEventListener("click", addListAfterClick);
